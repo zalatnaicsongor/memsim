57e4284524cef1574e50065b54943301dab0ce09
kadari <ktorpi@ktorpi-desktop.(none)>
2010-04-08 11:13:28 +0200

    PegaReplacer átnevezve PageReplaceStrategyre, hogy jobban hasonlitson a Cache-re. Modosítások a Page, Memory és VirtMemory osztályokban.

---
+++ src/memsim_java/PageFaultException.java
@@ -0,0 +1,11 @@
+package memsim_java;
+
+/**
+ * Laphiány-t reprezentáló osztály
+ * @author zealot
+ */
+public class PageFaultException extends Exception {
+    public PageFaultException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/memsim_java/PageReplaceStrategy.java
@@ -0,0 +1,23 @@
+package memsim_java;
+
+import java.util.LinkedList;
+
+/**
+ * Minden lapcserélő interfésze
+ * @author zealot
+ */
+public interface PageReplaceStrategy {
+    /**
+     * Ez ahhoz kell, hogy ha használunk egy lapot, akkor az megfelelően lekönyvelődjön
+     * (usageCount++, linkedlist elejére kerül...)
+     * @param PhysMem
+     */
+    public void doTheAccounting(Page used, LinkedList<Page> PhysMem);
+
+    /**
+     * ez meg visszaadja, hogy ez az alg. szerint melyiket kell kidobni...
+     * @param PhysMem
+     * @return
+     */
+    public Page whichToThrowOut(LinkedList<Page> PhysMem);
+}
